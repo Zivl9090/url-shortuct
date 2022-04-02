6d012dcc2c5f0801f0b055b5742d1f72e13b1cdf
Ziv Levi <zivlevi@Zivs-MBP.lan>
2022-04-02 10:25:55 +0300

    up

--- api/main.go
+++ api/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"url-shortcut/api/routes"
+	"url-shortcut/cache"
 	"url-shortcut/db"
 
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
 )
 
 func main() {
 	db.Connect()
 	db.Init()
+	cache.Connect()
 
 	app := fiber.New(fiber.Config{
 		ServerHeader: "Url shortcut v1.0.0",
 	})
+
+	routes.R_login(app.Group("/login"))
+
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("secret"),
+	}))
 
 	routes.R_url(app.Group("/url"))
 	routes.R_user(app.Group("/user"))
